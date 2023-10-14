66ca6225116411ce6d9b8876da487a55f3dab4f3
Kadifa <Kudickadifa@gmail.com>
2023-10-13 21:52:36 -0500

    Double colon operator

--- src/main/java/day3_doubleColon_and_coparator/Comparator.java
+++ src/main/java/day3_doubleColon_and_coparator/Comparator.java
@@ -1,9 +1,10 @@
 package day3_doubleColon_and_coparator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collections;
+//import java.util.List;
+import java.util.*;
 
 public class Comparator {
     public static void main(String[] args) {
@@ -11,34 +12,35 @@ public class Comparator {
         List<Integer> nums = new ArrayList<>(Arrays.asList(4, 2, 1, 6, 21, 5, 2, 10, 14));
       nums.forEach(System.out::println);
 
-        System.out.println("Sort in Ascending  order ------------");
+      System.out.println("----------- sort in ascending order ----------- ");
+      Collections.sort(nums);
+      nums.forEach(System.out::println);
 
-        Collections.sort(nums);
-        nums.forEach(System.out::println);
+      System.out.println("----------- sort in descending order ----------- ");
+      Collections.sort(nums, (n1, n2) -> n1 > n2 ? -1 : n1 < n2 ? 1 : 0);
+      nums.forEach(System.out::println);
 
-        System.out.println("Sort in Descending   order ------------");
-        Collections.sort(nums, (n1, n2) -> n1 > n2 ? -1 : n1 < n2 ? 1: 0);// logic behind compare()
-        nums.forEach(System.out::println);
+      System.out.println("----------- sort in ascending order again ----------- ");
+      Collections.shuffle(nums);
+      nums.sort((n1, n2) -> n1.compareTo(n2)); // by default sort method is in ascending
+      nums.forEach(System.out::println);
 
-        System.out.println("------------Sort in Ascending    order ------------");
-        Collections.shuffle(nums);
+      System.out.println("----------- sort in descending order again ----------- ");
+      Collections.shuffle(nums);
+      nums.sort((n2, n1) -> n1.compareTo(n2));
+      nums.forEach(System.out::println);
 
-        Collections.sort(nums, (n1, n2) -> n1.compareTo(n2));// logic behind compareTo()
-        nums.forEach(System.out::println);
+      System.out.println("----------- best way for descending ----------- ");
+      Collections.shuffle(nums);
+     // nums.sort(Comparator.reverseOrder()); // sort the opposite of natural order (ascending)
+      nums.forEach(System.out::println);
 
-        System.out.println("------------Sort in Descending   order ------------");
-        Collections.shuffle(nums);
-
-        Collections.sort(nums, (n2, n1) -> n1.compareTo(n2));// logic behind compareTo()
-        nums.forEach(System.out::println);
-
-        System.out.println("------------best way to do it  descending  ------------");
-        Collections.shuffle(nums);
-       // nums.sort(Comparator.reverseOrder());
-        nums.forEach(System.out::println);
+      // summary: to sort in ascending order we just use sort(), and to sort in descending we can call the reverseOrder() method from Comparator
 
 
 
     }
+
+
 
 }

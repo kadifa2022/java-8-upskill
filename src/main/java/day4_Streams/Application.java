cef74da1f2d4dda7cbb4f14130a44d49ee4ef17e
Kadifa <Kudickadifa@gmail.com>
2023-10-16 13:52:12 -0500

    Double colon operator

---
+++ src/main/java/day4_Streams/Application.java
@@ -0,0 +1,18 @@
+package day4_Streams;
+
+import lombok.AllArgsConstructor;// to add some data
+import lombok.Data;
+
+
+@AllArgsConstructor
+@Data// setter, getter, to String ()
+
+public class Application {
+    private String name;
+    private double expectedSalary;
+    private boolean willingToRelocate;
+
+
+
+
+}

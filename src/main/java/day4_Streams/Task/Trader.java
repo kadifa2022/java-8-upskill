425012c598ab3e77b5eaba8a4391f739fc9bb125
Kadifa <Kudickadifa@gmail.com>
2023-10-17 17:56:42 -0500

    Double colon operator

---
+++ src/main/java/day4_Streams/Task/Trader.java
@@ -0,0 +1,11 @@
+package day4_Streams.Task;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Trader {
+    private String name;
+    private String city;
+}

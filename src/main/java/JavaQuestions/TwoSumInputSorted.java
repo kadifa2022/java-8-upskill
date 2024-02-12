01857d7774d2ae1942f38a41cb95d9682e59dd40
Kadifa <Kudickadifa@gmail.com>
2024-02-11 21:36:08 -0600

    practice

--- src/main/java/JavaQuestions/TwoSumInputSorted.java
+++ src/main/java/JavaQuestions/TwoSumInputSorted.java
@@ -2,28 +2,40 @@ package JavaQuestions;
 
 public class TwoSumInputSorted {
     public static void main(String[] args) {
-    //int [] nums = {2,7,11,15};
-//        int target = 13;
+    int [] numbers = {2,7,11,15};
+        int target = 13;
 
-
-
+       // System.out.println(twoSum(numbers, target));
 
 
     }
+    // another solution
+    public static  int [] twoSum(int[]numbers , int target){
+        int left =0, right = numbers.length-1;
 
-    public static int [] twoSum(int[] nums, int target){
-        int start = 0, end = nums.length-1;
-
-        while(start< end){
-            if(nums[start] + nums[end]==target)
-                return new int[]{start +1, end +1};
-            if(nums[start] + nums[end]>target){
-                end--;
-            }else if(nums[start] + nums[end]<target){
-                start++;
-            }
+        while(numbers[left] + numbers[right] !=target){
+            if (numbers[left] + numbers[right] > target)
+                right--;
+            else
+                left ++;
+            if(right == left) return new int[]{};
         }
-        return null;
+        return new int[] {left +1, right +1};
     }
+//
+//    public static int [] twoSum(int[] nums, int target){
+//        int start = 0, end = nums.length-1;
+//
+//        while(start< end){
+//            if(nums[start] + nums[end]==target)
+//                return new int[]{start +1, end +1};
+//            if(nums[start] + nums[end]>target){
+//                end--;
+//            }else if(nums[start] + nums[end]<target){
+//                start++;
+//            }
+//        }
+//        return null;
+//    }
 
 }

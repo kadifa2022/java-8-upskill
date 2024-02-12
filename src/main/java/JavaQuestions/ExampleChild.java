6da686a40c8e30c8b6c23f32bdf42928afa26a85
Kadifa <Kudickadifa@gmail.com>
2024-02-11 21:22:40 -0600

    practice

---
+++ src/main/java/JavaQuestions/ExampleChild.java
@@ -0,0 +1,13 @@
+package JavaQuestions;
+
+public class ExampleChild  extends Example{
+
+
+    public void printStuff(int x, String s){
+        System.out.println("I am the example child");
+    }
+
+
+
+
+    }
---
+++ src/main/java/JavaQuestions/Main.java
@@ -0,0 +1,13 @@
+package JavaQuestions;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ExampleChild  exampleChild = new ExampleChild();
+        exampleChild.printStuff(10);
+        exampleChild.printStuff(20,"Nina");
+
+        System.out.println(exampleChild);
+
+    }
+}

4cf11920e4125839d1d702bc7de4057797037f29
Kadifa <Kudickadifa@gmail.com>
2024-02-14 08:48:45 -0600

    practice

--- src/main/java/JavaQuestions/IsPalindrome.java
+++ src/main/java/JavaQuestions/IsPalindrome.java
@@ -23,4 +23,20 @@ public class IsPalindrome {
     }
 
 
+    public static boolean isPalindrome2(String s){
+
+        int left = 0, right = s.length()-1;
+
+        while(left< right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left ++;
+            right --;
+        }
+        return  true;
+    }
+
+
+
 }

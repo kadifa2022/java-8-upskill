2c82d63e5f326027ea782b590b3a73d516e0ad96
Kadifa <Kudickadifa@gmail.com>
2024-02-14 15:58:59 -0600

    practice

---
+++ src/main/java/JavaQuestions/DifferencesBetweenStatements.java
@@ -0,0 +1,13 @@
+package JavaQuestions;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class DifferencesBetweenStatements {
+    public static void main(String[] args) {
+
+        //System.out.println(1/0);// rises ArithmeticException
+      //  System.out.println(2.0/0);// creates Infinity (Infinity constants is available in wrapper classes of float, double class
+
+    }
+
+}

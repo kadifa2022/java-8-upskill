6f24f98bb0a9caab1a4d228ee2ec994eafb708b1
Kadifa <Kudickadifa@gmail.com>
2024-02-20 22:42:25 -0600

    practice

--- src/main/java/JavaQuestions/PalindromeChecker.java
+++ src/main/java/JavaQuestions/PalindromeChecker.java
@@ -2,6 +2,13 @@ package JavaQuestions;
 
 public class PalindromeChecker {
 
+    public static void main(String[] args) {
+        int x = 1231;
+        System.out.println(isPalindromeNumber(x));
+        x = 12321;
+        System.out.println(isPalindromeNumber(x));
+    }
+
 
     public static boolean isPalindromeNumber(int x){
         // convert the integer to a string
@@ -11,10 +18,14 @@ public class PalindromeChecker {
         int left = 0,right =original.length()-1;
 
         while(left<right){
-            if (original.charAt(left) != original.charAt(right));
+            // comparing characters at left and right positions
+            if (original.charAt(left) != original.charAt(right)) {
+                return false;// if characters not equal, is not palindrome
+            }
+            //move the pointers towards center
             left ++;
             right --;
-        }
+        }// if the loop completes without returning false, the integer is a palindrome
         return true;
     }
 

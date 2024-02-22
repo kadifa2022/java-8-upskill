5a5ce45af1fd9f752f989ebb125bd8b2a50d0d1e
Kadifa <Kudickadifa@gmail.com>
2024-02-21 18:52:39 -0600

    practice

--- src/main/java/JavaQuestions/stringPractice/ReverseString.java
+++ src/main/java/JavaQuestions/stringPractice/ReverseString.java
@@ -17,12 +17,28 @@ public class ReverseString {
     //output : olleh
     public static void main(String[] args) {
         String str = "Hello";
+
+        // Approach 1
         char[] chArr = str.toCharArray();
 
         for (int i = chArr.length - 1; i >= 0; i--) {
             System.out.print(chArr[i]);
-
-
         }
+
+        System.out.println("------- Approach2 with charAt()-------");
+
+        for(int i = str.length() -1; i >= 0; i--){
+            System.out.print(str.charAt(i));
+        }
+        System.out.println("-----Approach 3 by using StringBuffer----");
+
+        StringBuffer sb = new StringBuffer(str);
+        System.out.println(sb.reverse());
+
+        System.out.println("Approach4 by using StringBuilder---------");
+
+        StringBuilder sBuilder = new StringBuilder(str);
+        System.out.println(sBuilder.reverse());
+
     }
 }

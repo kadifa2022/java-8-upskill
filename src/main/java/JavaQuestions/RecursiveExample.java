738e379c2d8c4a5f52f9c1c2af7768f9a3ac1eaf
Kadifa <Kudickadifa@gmail.com>
2024-02-25 21:03:32 -0600

    practice

---
+++ src/main/java/JavaQuestions/RecursiveExample.java
@@ -0,0 +1,21 @@
+package JavaQuestions;
+
+public class RecursiveExample {
+    public static void main(String[] args) {
+        int number =5;
+        int result = sum(number);
+        System.out.println("Sum of numbers from 1 to " + number + " is :" + result);
+
+    }
+  public static  int sum(int num){
+        if(num == 1){
+            return 1;
+        }else{
+            return num + sum (num-1);
+        }
+
+    }
+
+
+
+}
